3f4e551304b899595d2b9dfcd4d511e960ddff44
Lien <ayalp.ende@gmail.com>
2025-06-07 19:30:16 +0200

    add observable-overview

---
+++ src/app/observable-overview/observable-overview.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ObservableOverview } from './observable-overview';
+
+describe('ObservableOverview', () => {
+  let component: ObservableOverview;
+  let fixture: ComponentFixture<ObservableOverview>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ObservableOverview]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ObservableOverview);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/observable-overview/observable-overview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-observable-overview',
+  imports: [],
+  templateUrl: './observable-overview.html',
+  styleUrl: './observable-overview.css'
+})
+export class ObservableOverview {
+
+}
