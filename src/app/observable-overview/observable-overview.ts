e696b34a12ade9164ff538f79b8fe2e921a57430
Lien <ayalp.ende@gmail.com>
2025-06-07 19:37:33 +0200

    add observable-overview

--- src/app/observable-overview/observable-overview.ts
+++ src/app/observable-overview/observable-overview.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-observable-overview',
+  selector: 'observable-overview',
   imports: [],
   templateUrl: './observable-overview.html',
   styleUrl: './observable-overview.css'
