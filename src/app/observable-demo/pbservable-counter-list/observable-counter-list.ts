74de101c2c1ff5ffa2e96940ccac5aae0d091da2
Lien <ayalp.ende@gmail.com>
2025-06-08 22:29:16 +0200

    improved observable-counter-list

--- src/app/observable-demo/pbservable-counter-list/observable-counter-list.ts
+++ src/app/observable-demo/pbservable-counter-list/observable-counter-list.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, signal, Signal, WritableSignal} from '@angular/core';
+import { Component, OnDestroy, signal, Signal, WritableSignal } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 import { MatButton } from '@angular/material/button';
 
@@ -14,39 +14,42 @@ export interface Counter {
   styleUrl: './observable-counter-list.scss',
 })
 export class ObservableCounterList implements OnDestroy {
-  protected readonly counters: Signal<Counter>[] = [];
+  protected counters = signal<Signal<Counter>[]>([]);
   protected readonly disableButton = signal(false);
   private subs: Subscription[] = [];
   private readonly numberOfMaxRunningCounters = 4;
 
   ngOnDestroy(): void {
-    this.subs.forEach((sub: Subscription) => {sub.unsubscribe();});
+    this.subs.forEach((sub: Subscription) => {
+      sub.unsubscribe();
+    });
   }
 
   protected startCounter() {
-    this.counters.push(signal({ counter: signal(0), isRunning: signal(true) }));
+    const counterSignal = signal({ counter: signal(0), isRunning: signal(true) });
+    this.counters.update(counters => [...counters, counterSignal]);
     this.updateDisableButtonState();
 
-    const index = this.counters.length - 1;
     const sub = interval(500).subscribe(value => {
-      const current = this.counters[index]();
-      current.counter.set(value);
+      counterSignal().counter.set(value);
       if (value > 10) {
-        current.counter.set(0);
-        current.isRunning.set(false);
+        this.stopCounter(counterSignal);
+        this.counters.update(counters => counters.filter(counter => counter !== counterSignal));
         sub.unsubscribe();
-        const numberOfRunningCounters = this.counters.filter(counter =>
-          counter().isRunning(),
-        ).length;
-        this.disableButton.set(numberOfRunningCounters > 4);
       }
     });
 
     this.subs.push(sub);
   }
 
+  private stopCounter(counterSignal: Signal<Counter>) {
+    counterSignal().counter.set(0);
+    counterSignal().isRunning.set(false);
+    this.updateDisableButtonState();
+  }
+
   private updateDisableButtonState() {
-    const numberOfRunningCounters = this.counters.filter(counter => counter().isRunning()).length;
+    const numberOfRunningCounters = this.counters().filter(counter => counter().isRunning()).length;
     this.disableButton.set(numberOfRunningCounters > this.numberOfMaxRunningCounters);
   }
 }
