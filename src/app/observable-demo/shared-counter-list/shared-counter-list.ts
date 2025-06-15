7b0f301618f53c4950c06141d86d67a8a33db845
Lien <ayalp.ende@gmail.com>
2025-06-15 13:48:08 +0200

    add stop counter functionality

--- src/app/observable-demo/shared-counter-list/shared-counter-list.ts
+++ src/app/observable-demo/shared-counter-list/shared-counter-list.ts
@@ -3,8 +3,14 @@ import { interval, share, Subscription } from 'rxjs';
 import { MatButton } from '@angular/material/button';
 
 export interface CounterWrapper {
+  name: string;
   counter: WritableSignal<number | null>;
   isRunning: WritableSignal<boolean>;
+}
+
+export interface CounterSubscriptionMap {
+  counter: WritableSignal<CounterWrapper>;
+  subscription: Subscription;
 }
 
 @Component({
@@ -16,37 +22,55 @@ export interface CounterWrapper {
 export class SharedCounterList implements OnDestroy {
   protected counters = signal<Signal<CounterWrapper>[]>([]);
   protected readonly disableButton = signal(false);
-  private subs: Subscription[] = [];
+  private readonly counterSubscriptionMap: CounterSubscriptionMap[] = [];
   private readonly numberOfMaxRunningCounters = 4;
   private sharedCounter$ = interval(500).pipe(share());
 
   ngOnDestroy(): void {
-    this.subs.forEach((sub: Subscription) => {
-      sub.unsubscribe();
-    });
+    for (const entry of this.counterSubscriptionMap) {
+      const subscription: Subscription = entry.subscription;
+      subscription.unsubscribe();
+    }
+  }
+  protected randomName(): string {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < 3; i++) {
+      const index = Math.floor(Math.random() * chars.length);
+      result += chars[index];
+    }
+    return result;
   }
 
   protected startCounter() {
-    const counterSignal = signal({ counter: signal<number | null>(null), isRunning: signal(true) });
+    const counterSignal: WritableSignal<CounterWrapper> = signal({
+      name: this.randomName(),
+      counter: signal<number | null>(null),
+      isRunning: signal(true),
+    });
     this.counters.update(counters => [...counters, counterSignal]);
     this.updateDisableButtonState();
 
     const sub = this.sharedCounter$.subscribe(value => {
       counterSignal().counter.set(value);
-      if (value > 10) {
-        this.stopCounter(counterSignal);
-        this.counters.update(counters => counters.filter(counter => counter !== counterSignal));
-        sub.unsubscribe();
-      }
+      console.log(value);
     });
 
-    this.subs.push(sub);
+    this.counterSubscriptionMap.push({ counter: counterSignal, subscription: sub });
+
   }
 
-  private stopCounter(counterSignal: Signal<CounterWrapper>) {
+  protected stopCounter(counterSignal: Signal<CounterWrapper>) {
     counterSignal().counter.set(0);
     counterSignal().isRunning.set(false);
     this.updateDisableButtonState();
+    this.counters.update(counters => counters.filter(counter => counter !== counterSignal));
+    const counterIndex= this.counterSubscriptionMap.findIndex(entry => entry.counter === counterSignal);
+
+    if(counterIndex !== -1){
+      this.counterSubscriptionMap[counterIndex].subscription.unsubscribe();
+      this.counterSubscriptionMap.splice(counterIndex, 1);
+    }
   }
 
   private updateDisableButtonState() {
