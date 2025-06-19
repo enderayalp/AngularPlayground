e421d7b4def328bbef9a8069ab457a2dfb363703
Lien <ayalp.ende@gmail.com>
2025-06-19 22:31:40 +0200

    add table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ObservableOverview } from './observable-demo/observable-overview/observable-overview';
 import {SignalDemo} from './signal-demo/signal-demo.component';
+import {CustomTable} from './table-demo/custom-table';
 
 export const routes: Routes = [{ path: '', component: ObservableOverview, pathMatch: 'full' }
-,{path: 'signal-demo', component:SignalDemo, pathMatch: 'full'}];
+,{path: 'signal-demo', component:SignalDemo},
+  {path: 'table-demo', component:CustomTable}];
--- src/app/table-demo/custom-table.ts
+++ src/app/table-demo/custom-table.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-custom-table',
   imports: [],
   templateUrl: './custom-table.html',
-  styleUrl: './custom-table.scss'
+  styleUrl: './custom-table.scss',
 })
-export class CustomTable {
+export class CustomTable implements OnInit {
+  @Input() columns: string[] = [];
+  @Input() data: string[][] = [];
 
+  ngOnInit() {
+    this.columns = ['name', 'email', 'phone', 'status'];
+    this.data = [
+      ['ender', 'ayalp.ender@gmail.com', '017612303105', 'müde'],
+      ['barbara', 'sanfilippo.ayalp@gmail.com', '01722061569', 'genervt'],
+    ];
+  }
 }
